199b5fb3ae165c0a59e5c3690f9262fdf0efe499
Amineee-s <aminesane05@gmail.com>
2025-06-08 19:32:30 +0000

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+# Écrivez votre code ici !nom = "Amine"
+age = 19
+print(f"Bonjour je m'appelle ,  {nom}, et j'ai {age}ans")
+age = 29
+print(f"Bonjour je m'appelle ,  {nom}, et j'ai {age}ans")
